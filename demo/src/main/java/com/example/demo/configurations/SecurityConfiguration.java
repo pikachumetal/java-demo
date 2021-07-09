20dc7f0c726c60a7e853647b0d539c8ad058a9ad
Àngel Delgado <adelgado@medysif.com>
2021-07-09 15:19:57 +0200

    auth final

---
+++ demo/src/main/java/com/example/demo/configurations/SecurityConfiguration.java
@@ -0,0 +1,96 @@
+package com.example.demo.configurations;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.jwt.*;
+import org.zalando.problem.spring.web.advice.security.SecurityProblemSupport;
+
+@EnableWebSecurity
+@Import(SecurityProblemSupport.class)
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private SecurityProblemSupport problemSupport;
+
+    @Value("${spring.security.oauth2.resourceserver.opaque.introspection-uri}")
+    String oauthIntrospection;
+
+    @Value("${spring.security.oauth2.resourceserver.opaque.introspection-client-id}")
+    String clientId;
+
+    @Value("${spring.security.oauth2.resourceserver.opaque.introspection-client-secret}")
+    String clientSecret;
+
+    @Value("${auth0.audience}")
+    private String audience;
+
+    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+    private String issuer;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+//        // Enable CORS and disable CSRF
+//        http = http
+//                .cors().and()
+//                .csrf().disable();
+//
+//        // Other security-related configuration
+//        http = http
+//                .exceptionHandling()
+//                .authenticationEntryPoint(problemSupport)
+//                .accessDeniedHandler(problemSupport)
+//                .and();
+//
+//        // Set session management to stateless
+//        http = http
+//                .sessionManagement()
+//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//                .and().formLogin().disable();
+//
+//        // Set permissions on endpoints
+//        http.authorizeRequests(authorize -> authorize
+//                // Our public endpoints
+//                .mvcMatchers("/v1/**").permitAll()
+//                // Our private endpoints
+//                .anyRequest().authenticated())
+//                                .oauth2ResourceServer()
+//                .jwt()
+//                .decoder(jwtDecoder());
+////                .and()
+////                .opaqueToken()
+////                .introspectionClientCredentials(clientId, clientSecret)
+////                .introspectionUri(oauthIntrospection);
+        http
+                .httpBasic().disable()
+                .formLogin(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeRequests(authorize -> authorize
+                        // Our public endpoints
+                        //.mvcMatchers("/v1/**").permitAll()
+                        // Our private endpoints
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(o->o.jwt().decoder(jwtDecoder()))
+                .sessionManagement(sessionManagement ->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        ;
+    }
+
+    JwtDecoder jwtDecoder() {
+        OAuth2TokenValidator<Jwt> withAudience = new AudienceValidator(audience);
+        OAuth2TokenValidator<Jwt> withIssuer = JwtValidators.createDefaultWithIssuer(issuer);
+        OAuth2TokenValidator<Jwt> validator = new DelegatingOAuth2TokenValidator<Jwt>(withAudience, withIssuer);
+
+        NimbusJwtDecoder jwtDecoder = (NimbusJwtDecoder) JwtDecoders.fromOidcIssuerLocation(issuer);
+        jwtDecoder.setJwtValidator(validator);
+        return jwtDecoder;
+    }
+}
